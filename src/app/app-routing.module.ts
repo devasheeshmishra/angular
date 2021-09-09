c1188db34fae228f52a37fc27deaf8e1d7d3b891
devasheeshmishra <devasheeshmishra@gmail.com>
2021-09-09 14:24:49 +0530

    1st commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'portal', loadChildren: () => import('./core/core.module').then(m => m.CoreModule)},
+  { path: '', redirectTo: 'portal/checklist/all', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
